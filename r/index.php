657cf15ffddee3f15966cec2ad947f92f71b355d
ghostknow <tck1130@gmail.com>
2020-04-24 00:23:50 +0800

    Update

--- r/index.php
+++ r/index.php
@@ -1,5 +1,19 @@
 <!DOCTYPE html>
 <html>
+<head>
+    <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coming Soon</title>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"
+        integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="https://unpkg.com/popper.js@1.12.3"></script>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <style>
+        .w3-btn {
+            margin-bottom: 10px;
+        }
+    </style>
+</head>
+
 <body>
 <?php
 if (isset($_GET['d'])) {
