09fb6bd78a525b3fc20d558c0d8df8fe34214347
ghostknow <tck1130@gmail.com>
2020-04-24 00:25:56 +0800

    Update

--- r/index.php
+++ r/index.php
@@ -1,36 +1,6 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Coming Soon</title>
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js"
-        integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-    <script type="text/javascript" src="https://unpkg.com/popper.js@1.12.3"></script>
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <style>
-        .w3-btn {
-            margin-bottom: 10px;
-        }
-    </style>
-</head>
 
-<body>
-<?php
-if (isset($_GET['d'])) {
-    //echo $_GET['d'];
-
-    <script>
-        document.getElementById("demo").innerHTML =
-            "https://filedn.com/lGQxqDqSgANYGgE8J2NgzIV/" + $_GET['d']);
-            //"https://filedn.com/lGQxqDqSgANYGgE8J2NgzIV" + ((window.location.hostname + window.location.pathname).replace('D:/GitHub/ghostknow.github.io/r/', ''));
-            //"The page url is: " + window.location.hostname + window.location.pathname;
-    </script>
-}?>
-</body>
-</html>
-
-
-<!--!>
 <head>
     <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Coming Soon</title>
@@ -61,3 +31,5 @@ if (isset($_GET['d'])) {
 
     </iframe>
 </body>
+
+</html>
